bf6efb1be98f6583fb4fbc839273e8617a7c04c1
khaymanii <kennethakpo61@gmail.com>
2024-06-27 02:21:50 +0100

    fixed mobile responsiveness

--- app/components/Networkbuilder.tsx
+++ app/components/Networkbuilder.tsx
@@ -1,7 +1,51 @@
 import Link from "next/link";
 import React from "react";
 
-function Networkbuilder() {
+interface CardProps {
+  title: string;
+  description: string;
+  details: string;
+  link: string;
+}
+
+const InfoCard: React.FC<CardProps> = ({
+  title,
+  description,
+  details,
+  link,
+}) => (
+  <div className="bg-white rounded-xl w-full lg:w-[48%] px-6 md:px-8 py-4 mb-8 lg:mb-0 flex flex-col">
+    <h1 className="text-xl md:text-2xl lg:text-2xl font-bold">{title}</h1>
+    <div className="py-4 flex-grow">
+      <p className="text-gray-700 pb-2">{description}</p>
+      <p className="font-medium pb-4">{details}</p>
+    </div>
+    <Link className="font-bold underline mt-auto" href={link}>
+      Learn More
+    </Link>
+  </div>
+);
+
+const NetworkBuilder: React.FC = () => {
+  const cards = [
+    {
+      title: "Work with Service incubators to expedite your time-to-market",
+      description:
+        "Collaborators with our meticulously chosen service partners, each with a vested interest, ensuring a shared commitment to success in the game of venture building.",
+      details:
+        "For 7.5% equity, you get a product manager, product designer, frontend engineer, software engineer and growth marketer to build the MVP of your app or web application and validate it.",
+      link: "#",
+    },
+    {
+      title: "Access funding after your MVP is validated",
+      description:
+        "Startups that have built and validated their product can take advantage of the financial resources of the Investors Network elevating their potential for success in the venture capital landscape.",
+      details:
+        "Raise $5k, $25k, $50k and $125k in 4 bootstrap deals within 12 months.",
+      link: "#",
+    },
+  ];
+
   return (
     <div className="mx-auto max-w-6xl mb-16 px-4">
       <div className="mx-auto text-center mb-10">
@@ -11,49 +55,18 @@ function Networkbuilder() {
         </h1>
       </div>
       <div className="flex flex-col lg:flex-row justify-between items-center lg:items-stretch">
-        <div className="bg-white rounded-xl w-full lg:w-[48%] px-6 md:px-8 py-4 mb-8 lg:mb-0 flex flex-col">
-          <h1 className="text-xl md:text-2xl lg:text-2xl font-bold">
-            Work with Service incubators to expedite your time-to-market
-          </h1>
-          <div className="py-4 flex-grow">
-            <p className="text-gray-700 pb-2">
-              Collaborators with our meticulously chosen service partners, each
-              with a vested interest, ensuring a shared commitment to success in
-              the game of venture building.
-            </p>
-            <p className="font-medium pb-4">
-              For 7.5% equity, you get a product manager, product designer,
-              frontend engineer, software engineer and growth marketer to build
-              the MVP of your app or web application and validate it.
-            </p>
-          </div>
-          <Link className="font-bold underline mt-auto" href="#">
-            Learn More
-          </Link>
-        </div>
-        <div className="bg-white rounded-xl w-full lg:w-[48%] px-6 md:px-8 py-4 flex flex-col">
-          <h1 className="text-xl md:text-2xl lg:text-2xl font-bold">
-            Access funding after your MVP is validated
-          </h1>
-          <div className="py-4 flex-grow">
-            <p className="text-gray-700 pb-2">
-              Startups that have built and validated their product can take
-              advantage of the financial resources of the Investors Network
-              elevating their potential for success in the venture capital
-              landscape.
-            </p>
-            <p className="font-medium pb-4">
-              Raise $5k, $25k, $50k and $125k in 4 bootstrap deals within 12
-              months.
-            </p>
-          </div>
-          <Link className="font-bold underline mt-auto" href="#">
-            Learn More
-          </Link>
-        </div>
+        {cards.map((card, index) => (
+          <InfoCard
+            key={index}
+            title={card.title}
+            description={card.description}
+            details={card.details}
+            link={card.link}
+          />
+        ))}
       </div>
     </div>
   );
-}
+};
 
-export default Networkbuilder;
+export default NetworkBuilder;
--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar";
 import Network from "./components/Network";
 import Networkbuilder from "./components/Networkbuilder";
 import AreaOfFocus from "./components/AreaOfFocus";
+import Concept from "./components/Concept";
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
       <Network />
       <Networkbuilder />
       <AreaOfFocus />
+      <Concept />
     </div>
   );
 }
